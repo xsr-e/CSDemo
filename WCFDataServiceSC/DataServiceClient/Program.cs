1665a6da072d9ce9f906490153e4f0b34960213c
xsr-e <altkom@live.com>
2017-05-14 22:12:04 +0200

    WCF Data Service + Client

---
+++ WCFDataServiceSC/DataServiceClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Service Context
+            SchoolDataServiceContext.SchoolDBEntities svc
+                = new SchoolDataServiceContext.SchoolDBEntities(
+                    new Uri("http://localhost:1030/SchoolData.svc/"));
+
+
+            var queryTeacher = from t in svc.Teachers
+                               where t.Class == "4B"
+                               select t;
+
+            var firstTeacher = queryTeacher.FirstOrDefault();
+            
+            //Lazy load Students property
+            svc.LoadProperty(firstTeacher, "Students");
+            var firstTeacherStudents = from s in firstTeacher.Students
+                                       select s;
+
+            firstTeacherStudents
+                .ToList()
+                .ForEach(
+                s =>
+                {
+                    Console.WriteLine("Id {0} , FullName {2}, {1} ", s.Id, s.FirstName, s.LastName);
+                    Console.WriteLine("Teacher FullName {1}, {0}",firstTeacher.FirstName, firstTeacher.LastName);
+                }
+                );
+
+
+        }
+    }
+}
