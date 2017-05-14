c6de2ac38649a2428dd446ab02b5c5f227312214
xsr-e <altkom@live.com>
2017-05-14 22:07:55 +0200

    solution cleanup

---
+++ demo/IfCaseForSwitch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfCaseForSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+    //1. if
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            if (today == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("weekend");
+            }
+            else if (today == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("weekend");
+            }
+            else
+            {
+                Console.WriteLine("workday");
+            }
+
+    
+    //2. switch            
+            //auto-expanding enum types:
+            //switch snippet + [Tab], today variable + [RightArrow]
+            
+            switch (today)
+            {
+                case DayOfWeek.Friday:
+                    break;
+                case DayOfWeek.Monday:
+                    break;
+                case DayOfWeek.Saturday:  //Switch statement fallthrough (intended)
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("weekend");
+                    break;
+                case DayOfWeek.Thursday:
+                    break;
+                case DayOfWeek.Tuesday:
+                    break;
+                case DayOfWeek.Wednesday:
+                    break;
+                default:
+                    break;
+            }
+       
+
+    //3. For     
+
+            for (int i = 10; i > 2; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+    //4. for each (requires IEnumerable)            
+            foreach (var c in "collection".ToCharArray())
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
---
+++ demo/InterfacesComparableComparer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesComparableComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pp = new Person[]{
+                new Person {FirstName="Zygmunt", LastName="Nowak"}
+                ,new Person {FirstName="Anna", LastName="Nowak"}    
+                ,new Person {FirstName="Jan", LastName="Kowalski"}
+            };
+
+            Array.Sort(pp);
+            Console.WriteLine(pp[0]);
+            Array.Sort(pp, new PersonFirstNameComparer());
+            Console.WriteLine(pp[0]);
+            Array.Sort(pp, new PersonLastNameComparer());
+            Console.WriteLine(pp[0]);
+
+        }
+
+    }
+
+    public class Person : IComparable, IComparable<Person>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CompareTo(object obj)
+        {
+            var pOther = obj as Person;
+            return CompareTo(pOther);
+        }
+        
+        public int CompareTo(Person other)
+        {
+
+            var otherFull = other.LastName + other.FirstName;
+            var full = this.FirstName + this.LastName;
+
+            return full.CompareTo(otherFull);
+        }
+        public override string ToString()
+        {
+            return LastName + " " + FirstName;
+        }
+    }
+
+    class PersonLastNameComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.LastName.CompareTo(y.LastName);
+        }
+    }
+    class PersonFirstNameComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.FirstName.CompareTo(y.FirstName);
+        }
+    }
+
+}
